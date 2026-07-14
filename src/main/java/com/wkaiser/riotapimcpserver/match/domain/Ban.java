89955220cdebb7ae23f85fb0a0484e1a6a23e0a0
Wade Kaiser <contact.kaiser.wade@gmail.com>
2026-07-14 17:26:04 -0400

    test: add WireMock test for RiotMatchAdapter

--- src/main/java/com/wkaiser/riotapimcpserver/match/domain/Ban.java
+++ src/main/java/com/wkaiser/riotapimcpserver/match/domain/Ban.java
@@ -1,13 +1,17 @@
 package com.wkaiser.riotapimcpserver.match.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Represents a champion ban in champion select phase.
  */
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Ban {
     private int championId;
     private int pickTurn;
